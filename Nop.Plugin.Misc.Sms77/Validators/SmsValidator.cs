acb41f51933385fad29fd11d28951e5c4b523fe1
André <matthiez@gmail.com>
2020-12-18 13:12:13 +0100

    sms: added validator rules for params foreign_id + label

--- Nop.Plugin.Misc.Sms77/Validators/SmsValidator.cs
+++ Nop.Plugin.Misc.Sms77/Validators/SmsValidator.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using FluentValidation;
 using Nop.Plugin.Misc.Sms77.Models;
 
@@ -5,11 +6,16 @@ namespace Nop.Plugin.Misc.Sms77.Validators {
     public partial class SmsValidator : BaseAbstractMessageValidator<SmsModel> {
         public SmsValidator() : base(1520) {
             RuleFor(m => m.ForeignId)
+                .Matches(new Regex(@"[0-9a-z-@_.\\]", RegexOptions.IgnoreCase))
                 .MaximumLength(64);
             
             RuleFor(m => m.From)
                 .MaximumLength(11)
                 .When(m => !Util.IsNumeric(m.From));
+            
+            RuleFor(m => m.Label)
+                .Matches(new Regex(@"[0-9a-z-@_.\\]", RegexOptions.IgnoreCase))
+                .MaximumLength(100);
             
             RuleFor(m => m.Ttl)
                 .GreaterThan(0);
